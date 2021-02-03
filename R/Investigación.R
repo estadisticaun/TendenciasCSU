a2f3f9c91877dc32d351238a29a96b3765337bde
Alberto Rodríguez R <albrodriguezr@unal.edu.co>
2021-02-02 20:45:26 -0500

    Nueva carga investigación

--- R/Investigación.R
+++ R/Investigación.R
@@ -34,16 +34,35 @@ Grupos_pais <- tibble(Variable = "GRUPOSP",
 
 # Plot
 
-
 Grupos_pais <- Plot.Series(datos = Grupos_pais, categoria = "GRUPOSP", col = c("#116BEE", "#E62272"),
             libreria = "highcharter", 
-            titulo = "Evolución Grupos de investigación categorizados en SCIENTI",
+            titulo = "Evolución grupos de investigación categorizados en SCIENTI",
             labelY = "Número de grupos de investigación",
-            estilo    = list(hc.Tema = 4, hc.Slider = TRUE,
+            estilo    = list(hc.Tema = 4, hc.Slider = FALSE,
                              hc.Credits = "Total País y total Universidad Nacional de Colombia, 2002 -2020")
             )
 
 # Salvar htmls
 
 Salvar(Grupos_pais, "Investigacion", "GI_pais.html")
+  
+          
+# Grupos Intrasedes ----
+
+# Grupos de Investigación ----
+
+Grupos_InterUN <- tibble(Variable = "GRUPOSINTERUN", 
+                      YEAR = rep(2020, times = 4),
+                      SEMESTRE = rep(1, length(YEAR)),
+                      Clase = c("Única sede", "2 Sedes", "3 Sedes", "4 o más sedes"),
+                      Total = c(466, 91, 17, 4))
+
+# Plot
+
+col <-   c("#6d6666", "#f15a24", "#0071bc", "#8cc63f") 
             
+Grupos_Inter <- Plot.Torta(datos = Grupos_InterUN, categoria = "GRUPOSINTERUN", 
+                           periodo = 1, colores = col,
+                           titulo    = "Grupos categorizados de investigación UNAL de acuerdo a las <br> sedes de base de sus integrantes",
+                           estilo    = list(hc.Tema = 4, hc.Credits = "Año 2020"))
+                 
