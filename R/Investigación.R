64fe7b7c7459074dd4a4769878ea8c1cbcc957ef
Alberto Rodríguez R <albrodriguezr@unal.edu.co>
2021-02-04 08:30:11 -0500

    programas

--- R/Investigación.R
+++ R/Investigación.R
@@ -96,7 +96,7 @@ Grupos_Inter <- Plot.Torta(datos = Grupos_InterUN, categoria = "GRUPOSINTERUN",
 
 Salvar(Grupos_Inter, "Investigacion", "GI_inter.html")
 
-# Grupos de investigación Áreas OCDE
+# Grupos de investigación Áreas OCDE ----
 
 Grupos_OCDE <- tibble(Variable = "GRUPOSOCDE", 
                          YEAR = rep(2020, times = 6),
@@ -124,4 +124,208 @@ Grupos_OCDEP <- Plot.Barras(datos = Grupos_OCDE,
                titulo    = "Distribución de grupos categorizados de investigación UNAL <br> por áreas de la OCDE",
               estilo    = list(hc.Tema = 4, hc.Credits = "Año 2020"))
 
-Salvar(Grupos_OCDEP, "Investigacion", "GI_ocde.html")                 
+Salvar(Grupos_OCDEP, "Investigacion", "GI_ocde.html") 
+
+
+# Revistas institucionales ----
+
+RevistasP <- tibble(Variable = "REVISTAS",
+                      YEAR = rep(2003:2020, each = 2),
+                      SEMESTRE = rep(1, length(YEAR)),
+                      Clase = rep(c("UNAL", "País"), times = length(YEAR)/2),
+                      Total = c(15,	62,	22,	119,	28,	156,	38,	202,	40,	221,	38,	263,
+                                42,	309,	44,	372,	45,	466,	44, 513,	39,	515,	43,	526,
+                                43,	526,	43,	526,	30,	244,	30,	246,	30,	246,	31,	275))
+
+# Plot
+
+Revistas <- Plot.Series(datos = RevistasP, categoria = "REVISTAS", col = c("#116BEE", "#E62272"),
+                           libreria = "highcharter",
+                           titulo = "Evolución de las revistas de la UNAL en el índice Bibliográfico PUBLINDEX",
+                           labelY = "Total Revistas",
+                           estilo    = list(hc.Tema = 4, hc.Slider = FALSE,
+                                            hc.Credits = "Comparación país. 2003-2020")
+)
+
+Salvar(Revistas, "Investigacion", "Revistas.html")
+
+
+
+# Clasificación Revistas ----
+
+RevistasCla <- tibble(Variable = "REVISTASCLA",
+                         YEAR = rep(2020, times = 4),
+                         SEMESTRE = rep(1, length(YEAR)),
+                         Clase = c("A1", "A2", "B", "C"),
+                         Total = c(0, 1, 12, 18))
+
+# Plot
+
+col <-   c("#6d6666", "#f15a24", "#0071bc", "#8cc63f")
+
+RevistasClaU <- Plot.Torta(datos = RevistasCla, categoria = "REVISTASCLA",
+                           periodo = 1, colores = col,
+                           titulo    = "Clasificación Revistas de la Universidad Nacional de Colombia",
+                           estilo    = list(hc.Tema = 4, hc.Credits = "Año 2020"))
+
+Salvar(RevistasClaU, "Investigacion", "RevistasClaU.html")
+
+# Productos Comité Puntaje ----
+
+Comite <- tibble(Variable = "TOTAL",
+                    YEAR = rep(2004:2019, each = 1),
+                    SEMESTRE = rep(1, length(YEAR)),
+                    Clase = rep(c("Total"), times = length(YEAR)),
+                    Total = c(3300, 3716, 4620, 5412, 5693, 5878, 6254, 6853,
+                              6758,	6120,	6259,	5924,	5888,	5997,	5449,	5532))
+
+ComiteP <- Plot.Series(datos = Comite, categoria = "TOTAL", col = c("#116BEE"),
+                        libreria = "highcharter",
+                        titulo = "Evolución de productos sometidos al Comité de Puntaje <br> de la Universidad Nacional de Colombia",
+                        labelY = "Total Productos",
+                        estilo    = list(hc.Tema = 4, hc.Slider = FALSE,
+                                         hc.Credits = "Periodo: 2004-2019"))
+
+Salvar(ComiteP, "Investigacion", "Comite.html")
+
+
+# Productos Comité Puntaje por tipologías ----
+
+Comite_Tipo <- tibble(Variable = "COMITET",
+                 YEAR = rep(2004:2019, each = 5),
+                 SEMESTRE = rep(1, length(YEAR)),
+                 Clase = rep(c("Creación artística", "Divulgación científica", "Formación", "Nuevo conocimiento","Premios"), times = length(YEAR)/5),
+                 Total = c(84, 602, 384, 2195, 35,
+                           94, 685, 413, 2491, 33,
+                           133, 884, 592, 2989, 22,
+                           126, 1130, 719, 3388, 49,
+                           129, 1299, 742, 3488, 35,
+                           101, 1487, 845, 3424, 21,
+                           124, 1545, 959, 3596, 30,
+                           108, 1416, 1324, 3993, 12,
+                           93, 1332, 1451, 3864, 18,
+                           89, 1186, 1165, 3661, 19,
+                           64, 1094, 1588, 3492, 21,
+                           66, 1070, 1610, 3167, 11,
+                           68, 1010, 1720, 3079, 11,
+                           45, 1039, 1882, 3029, 2,
+                           26, 856, 1842, 2722, 3,
+                           32, 670, 2299, 2525, 6))
+
+
+# Plot
+
+col <-   c( "#6d6666",  # gris
+            "#fbb03b", # amarillo
+            "#29abe2", # azul claro
+            "#c1272d",  # rojo
+            "#8cc63f")  # verde
+
+
+ComitePT <- Plot.Series(datos = Comite_Tipo, categoria = "COMITET", col = col,
+                       libreria = "highcharter",
+                       titulo = "Evolución de productos sometidos al Comité de Puntaje <br> de la Universidad Nacional de Colombia por tipología",
+                       labelY = "Total Productos",
+                       estilo    = list(hc.Tema = 4, hc.Slider = FALSE,
+                                        hc.Credits = "Periodo: 2004-2019"))
+     
+Salvar(ComitePT, "Investigacion", "Comitet.html")
+
+
+# Programas académicos ----
+
+
+Programas <- tibble(Variable = "TOTAL",
+                 YEAR = rep(1994:2020, each = 1),
+                 SEMESTRE = rep(1, length(YEAR)),
+                 Clase = rep(c("Total"), times = length(YEAR)),
+                 Total = c(189, 203, 211, 231, 247, 259, 267, 279, 303,
+                           308,	332, 344, 371, 386, 395, 425, 435, 438,
+                           425, 437, 453, 455, 444, 450, 455, 468, 469))
+
+ProgramasU <- Plot.Series(datos = Programas, categoria = "TOTAL", col = c("#116BEE"),
+                       libreria = "highcharter",
+                       titulo = "Evolución del número de programas académicos en la Universidad Nacional de Colombia",
+                       labelY = "Total programas académicos",
+                       estilo    = list(hc.Slider = FALSE,
+                                        hc.Credits = "Periodo: 1994-2020"))
+
+
+Salvar(ProgramasU, "Investigacion", "Programas.html")
+
+# Programas académicos por modalidad----
+
+ProgramasN <- tibble(Variable = "MODALIDAD",
+                    YEAR = rep(1994:2020, each = 2),
+                    SEMESTRE = rep(1, length(YEAR)),
+                    Clase = rep(c("Pregrado", "Postgrado"), times = length(YEAR)/2),
+                    Total = c(72, 117, 74, 129, 74, 137, 82, 149, 88, 159,
+                              91, 168, 93, 174, 94, 185, 94, 209, 94, 214,
+                              94, 238, 94, 250, 94, 277, 94, 292, 94, 301, 
+                              94, 331, 94, 341, 94, 344, 94, 331, 94, 343,
+                              94, 359, 94, 361, 95, 349, 95, 355, 95, 360,
+                              102, 366, 102, 367))
+
+# Plot
+
+ProgramasNivel <- Plot.Series(datos = ProgramasN, categoria = "MODALIDAD", col = c("#116BEE", "#E62272"),
+                        libreria = "highcharter",
+                        titulo = "Evolución del número de programas académicos por modalidad de formación",
+                        labelY = "Total programas académicos",
+                        estilo    = list(hc.Slider = FALSE, hc.Credits = "Periodo: 1994-2020"))
+
+
+Salvar(ProgramasNivel, "Investigacion", "Modalidad.html")
+
+
+# Programas académicos de postgrado ----
+
+Postgrado <- tibble(Variable = "POSTGRADO",
+                     YEAR = rep(1994:2020, each = 4),
+                     SEMESTRE = rep(1, length(YEAR)),
+                     Clase = rep(c("Especialización", "Especialidades médicas", "Maestría", "Doctorado"), times = length(YEAR)/4),
+                     Total = c(36, 30, 46, 5, 42, 30, 47, 10, 45, 31, 50, 11, 48, 31, 59, 11,
+                               54, 31, 63, 11, 56, 33, 66, 13, 54, 33, 74, 13, 59, 33, 79, 14,
+                               79, 33, 80, 17, 76, 34, 85, 19, 93, 34, 90, 21, 95, 35, 92, 28,
+                               97, 35, 113, 32, 105, 36, 116, 35, 110, 36, 120, 35, 115, 38, 132,
+                               46, 115, 38, 135, 53, 112, 38, 140, 54, 83, 39, 153, 56, 95, 39, 153,
+                               56, 101, 40, 160, 58, 98, 40, 161, 62, 80, 40, 165, 64, 83, 40, 167, 
+                               65, 84, 40, 168, 68, 88, 40, 169, 69, 89, 40, 169, 69))
+
+# Plot
+
+col <-   c( "#6d6666",  # gris
+            "#29abe2", # azul claro
+            "#c1272d",  # rojo
+            "#8cc63f")  # verde
+
+
+PostgradoU <- Plot.Series(datos = Postgrado, categoria = "POSTGRADO", col = col,
+                              libreria = "highcharter",
+                              titulo = "Evolución del número de programas académicos de postgrado",
+                              labelY = "Total programas académicos",
+                              estilo    = list(hc.Slider = FALSE, hc.Credits = "Periodo: 1994-2020"))
+
+Salvar(PostgradoU, "Investigacion", "Postgrado.html")
+
+
+# Investigadores por Sexo ----
+
+Inves_sexo <- tibble(Variable = "INVSEXO",
+                      YEAR = rep(2020, times = 2),
+                      SEMESTRE = rep(2, length(YEAR)),
+                      Clase = c("Hombres", "Mujeres"),
+                      Total = c(827, 365))
+
+# Plot
+
+col <-   c("#f15a24", "#8cc63f")
+
+Investigadores_sexo <- Plot.Torta(datos = Inves_sexo, categoria = "INVSEXO",
+                           periodo = 2, colores = col,
+                           titulo    = "Distribución de docentes investigadores por sexo",
+                           estilo    = list(hc.Credits = "Año 2020"))
+
+Salvar(Investigadores_sexo, "Investigacion", "InvSexo.html")
+
+
